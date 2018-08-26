1967bb18223cc20cdf5b6e9032a5c18ddccf910d
zunzelf <nugrohot3@gmail.com>
2018-08-27 06:59:43 +0700

    added LOG

--- app/src/main/java/zunzelf/org/far/MainActivity.java
+++ app/src/main/java/zunzelf/org/far/MainActivity.java
@@ -1,21 +1,27 @@
 package zunzelf.org.far;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.ColorSpace;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
@@ -29,23 +35,27 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 100;
+    private static final String TAG = "MainActivity";
     ImageView imageView;
+    ProgressDialog myDialog;
     Uri imageURI;
-    TextView txt;
-    Bitmap bitmap = null;
+    Bitmap bitmap;
     RGBArr rgbArr;
+    ScrollView sv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        txt = (TextView) findViewById(R.id.tVi);
+        sv = (ScrollView) findViewById(R.id.scrollView2);
+        sv.setVisibility(View.INVISIBLE);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openGallery();
+                sv.setVisibility(View.VISIBLE);
             }
         });
     }
@@ -68,9 +78,7 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
-
     }
 
     private void openGallery(){
@@ -87,14 +95,20 @@ public class MainActivity extends AppCompatActivity {
             // imageView.setImageURI(imageURI); to show image -> will uncomment when layout sorted
             // Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
             bitmap = null;
+            Log.d(TAG, "Load image");
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageURI);
+                bitmap = getResizedBitmap(bitmap, 400, 400);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            Log.d(TAG, "Image get!");
             // > Image Processes :
             // Get RGBs value per-pixel
+            // using thread
             rgbArr = getRGBs(bitmap);
+            //
+//            rgbArr = getRGBs(bitmap);
             // plotting to Graph :
             //  TODO : need to simplify plotting method
             GraphView graph;       //an Object of the PointsGraphSeries for plotting scatter graphs
@@ -117,6 +131,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(
+                bm, 0, 0, width, height, matrix, false);
+        bm.recycle();
+        return resizedBitmap;
+    }
+
     private void setLineGraphSeries(GraphView gv, int[] vArr, int color){
         gv.removeAllSeries();
         LineGraphSeries<DataPoint> series;
@@ -128,17 +159,20 @@ public class MainActivity extends AppCompatActivity {
         gv.removeAllSeries();
         BarGraphSeries<DataPoint> series;
         series= new BarGraphSeries<>(rgbToPlot(vArr));   //initializing/defining series
-//        series.setColor(color);
+        series.setColor(color);
         gv.addSeries(series);
+        gv.getViewport().setScalable(true);
     }
 
     private RGBArr getRGBs(Bitmap bitmap){
         RGBArr arr = new RGBArr();
         int w = bitmap.getWidth();
         int h = bitmap.getHeight();
-        System.out.println("width : "+w);
-        System.out.println("height : "+h);
+        Log.d(TAG, "width : "+w);
+        Log.d(TAG, "height : "+h);
         for (int y = 0; y < h; y++){
+//            int i = (int) Math.round(((y+0.0)/h)*100.0);
+//            myDialog.setProgress(i);
             for(int x = 0; x < w; x++){
                 int pixel = bitmap.getPixel(x, y);
                 int r = Color.red(pixel);
@@ -152,7 +186,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return arr;
-
     }
 
     public DataPoint[] rgbToPlot(int[] arr){
@@ -164,5 +197,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return values;
     }
-
 }
